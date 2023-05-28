440bbabc3d29129426cfe28872bb9711d5fa2944
g-ames <132623120+g-ames@users.noreply.github.com>
2023-05-28 14:07:38 -0400

    Update fix_iframes.js

--- fix_iframes.js
+++ fix_iframes.js
@@ -1,1 +1,9 @@
+// Get all the iframes on the page
+var iframes = document.getElementsByTagName('iframe');
 
+// Loop through each iframe and set width and height to 100%
+for (var i = 0; i < iframes.length; i++) {
+  var iframe = iframes[i];
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+}
