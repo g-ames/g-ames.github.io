776936c23ce620ab3291be6129dbe7e8b40c56c1
g-ames <132623120+g-ames@users.noreply.github.com>
2023-07-24 14:53:19 +0000

    Yay

---
+++ js/load_games.js
@@ -0,0 +1,37 @@
+const game_container = document.getElementById("games");
+
+async function loadGames() {
+    const games_json = (await (await fetch("/games/all.json")).text());
+    const games = JSON.parse(games_json);
+    for(i in games) {
+        // name, thumbnail, slug, desc
+        var game = games[i];
+
+        var game_subcontainer = document.createElement("div");
+
+        var game_name = document.createElement("label");
+        game_name.innerText = game["name"];
+        game_name.for = `thumbnail_${i}`;
+
+        var game_thumbnail = document.createElement("img");
+        game_thumbnail.src = game["thumbnail"];
+        game_thumbnail.id = `thumbnail_${i}`;
+
+        var game_desc = document.createElement("p");
+        game_desc.innerText = game["desc"];
+
+        var game_redirect = document.createElement("a");
+        game_redirect.href = `/games/game/${game["slug"]}`;
+
+        var fragment = document.createDocumentFragment();
+        fragment.appendChild(game_name);
+        fragment.appendChild(game_thumbnail);
+        fragment.appendChild(game_desc);
+        fragment.appendChild(game_redirect);
+
+        game_subcontainer.appendChild(fragment);
+        game_container.appendChild(game_subcontainer);
+    }
+}
+
+loadGames();
