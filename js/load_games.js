07e5c2df9fb961352315e28b35ed985dce5ee6eb
g-ames <132623120+g-ames@users.noreply.github.com>
2023-09-17 15:54:05 -0400

    Update load_games.js

--- js/load_games.js
+++ js/load_games.js
@@ -1,3 +1,30 @@
+// too lazy so I just copy pasted
+
+function linkToName(link) {
+    const cleanedLink = link.replace(/^(https?:\/\/)?(www\.)?/, ''); // Remove "http://" or "https://", and "www."
+    const parts = cleanedLink.split('/');
+    let name = parts.slice(-3).join('/').replace('index.html', '').replace(/\/+$/, '');
+        
+    if (parts.length > 3) {
+        name += ` (${parts.slice(0, -3).join(', ')})`;
+    }
+        
+    return name.charAt(0).toUpperCase() + name.slice(1);    
+}
+
+function setDetailSidebar(details) {
+    const { link, imagePath, description } = details;
+    const name = linkToName(link);
+
+    detailSidebarName.textContent = name;
+    detailSidebarThumbnail.src = imagePath;
+    detailSidebarDesc.textContent = description;
+    detailSidebarGo.href = link;
+    detailSidebarGo.children[0].textContent = `Go to ${name}!`;
+    detailSidebarGo.style.display = '';
+}
+
+
 async function loadGames() {
     const games_json = (await (await fetch("/games/all.json")).text());
     const games = JSON.parse(games_json);
