ab2bd0d93844de6db6123941cbec72d984a17700
g-ames <132623120+g-ames@users.noreply.github.com>
2023-09-17 15:51:10 -0400

    Update load_games.js

--- js/load_games.js
+++ js/load_games.js
@@ -1,38 +1,15 @@
-const game_container = document.getElementById("games");
-
 async function loadGames() {
     const games_json = (await (await fetch("/games/all.json")).text());
     const games = JSON.parse(games_json);
-    for(i in games) {
-        // name, thumbnail, slug, desc
-        var game = games[i];
 
-        var game_subcontainer = document.createElement("div");
-        game_subcontainer.classList = ["game_container"];
-    
-        var game_name = document.createElement("h3");
-        game_name.innerText = game["name"];
-        game_name.for = `thumbnail_${i}`;
-
-        var game_thumbnail = document.createElement("img");
-        game_thumbnail.src = game["thumbnail"];
-        game_thumbnail.id = `thumbnail_${i}`;
-
-        game_thumbnail.style.width = "204px";
-        game_thumbnail.style.height = "152px";
-
-        var game_desc = document.createElement("p");
-        game_desc.innerText = game["desc"];
-
-        var game_redirect = document.createElement("a");
-        game_redirect.href = `/games/game/${game["slug"]}`;
-
-        game_redirect.appendChild(game_name);
-        game_redirect.appendChild(game_thumbnail);
-        game_subcontainer.appendChild(game_redirect);
-
-        game_container.appendChild(game_subcontainer);
-    }
+    const gameDataList = games.map(game => {
+      return {
+        link: `/${game.slug}`, // Assuming the link should include the slug
+        imagePath: game.thumbnail || example_image, // Use example_image if thumbnail is empty
+        description: game.slug // Use the slug for description
+      };
+    });
+    createGameContainers(gameDataList);
 }
 
 loadGames();
